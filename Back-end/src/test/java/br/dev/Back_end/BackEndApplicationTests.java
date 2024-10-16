2fee5d7e96652fd9d4af3315b0a05bd8bf37b0d1
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2024-10-16 18:11:41 -0300

    Iniciando Projeto

---
+++ Back-end/src/test/java/br/dev/Back_end/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.Back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
